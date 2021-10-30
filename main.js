8ff9bb6f20d6b37aef73e7666bd3bbfd6fee9284
attackeralireza <sarbazvatan74@gmail.com>
2021-10-30 21:50:04 +0330

    end work for now

---
+++ main.js
@@ -0,0 +1,16 @@
+// Get Element
+var el = document.getElementById("email");
+// Specify the input type
+var eltype = document.querySelector("input").getAttribute("type");
+// Email Extension "You can change this"
+var Email_extension = "@" + "gmail.com";
+
+if (eltype == "email") {
+  el.addEventListener("keydown", function (event) {
+    var code = event.keyCode || event.which;
+    // Press Tab
+    if (code === 9) {
+        el.value += Email_extension;
+    }
+  });
+}
